7781adc7e22e6d47de848f1109b7bed3a0fc72db
Mangolopa <62287353+Mangolopa@users.noreply.github.com>
2020-03-18 11:00:27 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/waterapp/OverviewActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+    }
+
+    public void AddActivity(View view) {
+    }
+
+    public void PatientActivity(View view) {
+    }
+}
