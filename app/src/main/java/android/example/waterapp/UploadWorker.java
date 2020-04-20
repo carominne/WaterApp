4615d866a02ef398ee040082296eb423e94836a1
Mangolopa <christophe.mangolopa@student.uliege.be>
2020-04-20 10:21:02 +0200

    UploadWorker
    rework

--- app/src/main/java/android/example/waterapp/UploadWorker.java
+++ app/src/main/java/android/example/waterapp/UploadWorker.java
@@ -54,188 +54,60 @@ public class UploadWorker extends Worker {
     }
 
 
-    public Result doWork() {
+    public SharedPreferences doWork() {
 
-        public class OverviewActivity<jsonArray> extends AppCompatActivity  implements  View.OnClickListener {
+        request();
 
-        LinearLayout layout;
-        public static final int TEXT_REQUEST = 1;
-        private static final String LOG_TAG = "message";
-        public final static String EXTRA_PATIENT = "com.example.myexampleapp.PATIENT";
-        public String jpp ="";
-        public  String mResponse;
-        public Patient[] patients = null;
-        public ArrayList<Patient> pat ;
-        public Integer nb_patient = 0;
+        SharedPreferences m = PreferenceManager.getDefaultSharedPreferences(this);
+        mResponse = m.getString("Response", "");
 
-        // public VolleyCallback callback;
+        return m;
 
+    }
 
-        @RequiresApi(api = Build.VERSION_CODES.N)
+    public static final int TEXT_REQUEST = 1;
+    private static final String LOG_TAG = "message";
+    public final static String EXTRA_PATIENT = "com.example.myexampleapp.PATIENT";
+    public String jpp ="";
+    public  String mResponse;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_overview);
-            layout = findViewById(R.id.layout_overview);
+    public void request (){
+        try {
+            final String URL = "http://10.0.2.2:8080/demo/all"; // il faut mettre 10.0.2.2 pour avoir localhost dans l'émulateur andoid : http://10.0.2.2:8080/demo/all
 
 
-//   %%%%%%%%%%%%%%% JSONARRAY GET REQUEST
-            // Formulate the request and handle the response.
-            final String[] a = {""};
-            JSONArray json2 = null;
 
-            request();
-
-            SharedPreferences m = PreferenceManager.getDefaultSharedPreferences(this);
-            mResponse = m.getString("Response", "");
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, URL, null, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
 
 
-            Log.i(LOG_TAG, "coucou00788: " + mResponse);
+                    //Toast.makeText(getApplicationContext(), "I am OK !" + response.toString(), Toast.LENGTH_LONG).show();
+                    sharedResponse(response.toString());
+                    Log.i(LOG_TAG, "coucou wtf2: " + response);
 
-            String[] words = {};
-            if (mResponse.length() > 0) {
-                mResponse = mResponse.substring(1, mResponse.length() - 1);
+                    jpp = response.toString();
 
-                String[] json = {};
-
-                String line = mResponse;
-                words = line.split("\\}\\,");
-
-                Log.i(LOG_TAG, "coucou00788: " + words[0]);
-
-
-                patients = new Patient[words.length];
-
-                for (int i = 0; i < words.length; i++) {
-
-                    Log.i(LOG_TAG, "coucou0098: " + words[i] + "}");
-                    Patient patient = null;
-                    if (i == words.length - 1) {
-                        patient = new Gson().fromJson(words[i], new TypeToken<Patient>() {
-                        }.getType());
-                    } else {
-                        patient = new Gson().fromJson(words[i] + "}", new TypeToken<Patient>() {
-                        }.getType());
-                    }
-
-                    patients[i] = patient;
                 }
-
-
-                pat = new ArrayList<>(Arrays.asList(patients));
-                Log.i(LOG_TAG, "coucou: " + pat.get(0).getName());
-                pat.sort(new SortedPatient());
-
-                for (int i = 0; i < pat.size(); i++) {
-                    Patient patient = pat.get(i);
-                    patient.setButton(i);
-                    pat.set(i, patient);
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                    error.printStackTrace();
                 }
-
-                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(400, 200);
-                params.setMargins(75, 48, 24, 24);
-
-
-                for (int i = 0; i < words.length; i++) {
-
-                    final Button button = new Button(this);
-                    int remainder = i % 2;
-                    if (remainder == 0) {
-                        params.setMargins(75, 48, 24, 24);
-                        button.setLayoutParams(new LinearLayout.LayoutParams(params));
-                    }
-                    if (remainder == 1) {
-                        params.setMargins(554, -224, 75, 24);
-                        button.setLayoutParams(new LinearLayout.LayoutParams(params));
-                    }
-
-
-                    button.setId(pat.get(i).getButton());
-                    button.setText(pat.get(i).getName() + " " + pat.get(i).getForename() + " :" + pat.get(i).getRoom());
-
-
-                    if (pat.get(i).getDehydrationState().equals(0)) {
-                        button.setBackgroundColor(Color.GREEN);
-                    }
-
-                    if (pat.get(i).getDehydrationState().equals(1)) {
-                        button.setBackgroundColor(Color.YELLOW);
-                        Log.i(LOG_TAG, "red: " + pat.get(i).getDehydrationState());
-                    }
-
-                    if (pat.get(i).getDehydrationState().equals(2)) {
-                        button.setBackgroundColor(Color.RED);
-
-                    }
-
-                    button.setOnClickListener(this);
-
-
-                    layout.addView(button);
-                }
-            }
-
-
-            Intent intent = getIntent();
-
-            if (intent.getIntExtra("var", 1)==4) {
-                Log.i(LOG_TAG, "JE REDEMARRE");
-                Intent i = new Intent(OverviewActivity.this, OverviewActivity.class);
-                //finish();
-                overridePendingTransition(0, 0);
-                startActivity(i);
-                overridePendingTransition(0, 0);
-
-            }
-
+            });
+            VolleyController.getInstance(getApplicationContext()).addToRequestQueue(jsonArrayRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
-            public void request (){
-                try {
-                    final String URL = "http://10.0.2.2:8080/demo/all"; // il faut mettre 10.0.2.2 pour avoir localhost dans l'émulateur andoid : http://10.0.2.2:8080/demo/all
-
-
-
-                    JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, URL, null, new Response.Listener<JSONArray>() {
-                        @Override
-                        public void onResponse(JSONArray response) {
-
-
-                            //Toast.makeText(getApplicationContext(), "I am OK !" + response.toString(), Toast.LENGTH_LONG).show();
-                            sharedResponse(response.toString());
-                            Log.i(LOG_TAG, "coucou wtf2: " + response);
-
-                            jpp = response.toString();
-
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
-                            error.printStackTrace();
-                        }
-                    });
-                    VolleyController.getInstance(getApplicationContext()).addToRequestQueue(jsonArrayRequest);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-
-
-            public void sharedResponse(String response) {
-                SharedPreferences m = PreferenceManager.getDefaultSharedPreferences(this);
-                SharedPreferences.Editor editor = m.edit();
-                Log.i(LOG_TAG, "coucou0072: " + response);
-                editor.putString("Response", response);
-                editor.commit();
-            }
-
-        }
-
-        return Result.success()
-
+    public void sharedResponse(String response) {
+        SharedPreferences m = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = m.edit();
+        Log.i(LOG_TAG, "coucou0072: " + response);
+        editor.putString("Response", response);
+        editor.commit();
     }
 
 
