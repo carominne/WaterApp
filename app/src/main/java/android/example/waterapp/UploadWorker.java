984ea66b0455e7226d20865aa48cef6d967074b0
AmandineGrosfils <agrosfils@student.uliege.be>
2020-04-20 10:43:48 +0200

    coucou

--- app/src/main/java/android/example/waterapp/UploadWorker.java
+++ app/src/main/java/android/example/waterapp/UploadWorker.java
@@ -1,67 +1,47 @@
 package android.example.waterapp;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
-import android.view.Window;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+public class UploadWorker<context> extends Worker {
 
-import javax.xml.transform.Result;
 
-public class UploadWorker extends Worker {
-
+    private Object context2;
 
     public UploadWorker(
             @NonNull Context context,
             @NonNull WorkerParameters params) {
         super(context, params);
+        context2 = context;
+
     }
 
 
-    public SharedPreferences doWork() {
+
+    public Result doWork() {
 
         request();
 
-        SharedPreferences m = PreferenceManager.getDefaultSharedPreferences(this);
+
+        SharedPreferences m = PreferenceManager.getDefaultSharedPreferences((Context) context2);
         mResponse = m.getString("Response", "");
 
-        return m;
+        return (Result) m;
 
     }
 
@@ -103,7 +83,7 @@ public class UploadWorker extends Worker {
     }
 
     public void sharedResponse(String response) {
-        SharedPreferences m = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences m = PreferenceManager.getDefaultSharedPreferences((Context) context2);
         SharedPreferences.Editor editor = m.edit();
         Log.i(LOG_TAG, "coucou0072: " + response);
         editor.putString("Response", response);
